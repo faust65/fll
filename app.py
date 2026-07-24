900b7d3cdb993243a557ab991ec0b3dff006c57b
faust65 <151982135+faust65@users.noreply.github.com>
2026-07-24 20:53:45 +0900

    7

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/fll')
+def fll():
+    return render_template('fll.html')
+
+@app.route('/flll')
+def flll():
+    return render_template('flll.html')
+
 if __name__ == '__main__':
 
     app.run()
